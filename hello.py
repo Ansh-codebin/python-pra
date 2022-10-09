f85a28596c305736856c7840fb718b265cb87d34
Ansh Malviya <anshmalviya2634@gmail.com>
2022-10-09 13:34:55 +0530

    practices 1

---
+++ hello.py
@@ -0,0 +1,15 @@
+from tokenize import Number
+
+
+x=5
+y=6
+c=x+y
+print(c) 
+x=int(input("enter Number"))
+squre= x**0.5
+print("squre root of the number is :", squre)
+
+x=int(input("enter base"))
+y=int(input("enter height"))
+area = 1/2*x*y
+print("area of triangle is : ", area )
