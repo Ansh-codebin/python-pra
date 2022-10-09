4202ab2ac49a80dfbcdfa8a3f309d4380c0aa3f7
Ansh Malviya <anshmalviya2634@gmail.com>
2022-10-09 16:14:22 +0530

    practices

---
+++ elif.py
@@ -0,0 +1,10 @@
+x=int(input("enter the number"))
+y = int(input("enter the number"))
+z = int(input("enter the number"))
+if x>y and x>z :
+    print ("greatest")
+elif y>x and y>z:
+    print("greatest")
+elif z>x and z>y:
+    print("greatest")
+    
